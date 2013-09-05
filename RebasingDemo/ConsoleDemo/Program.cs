3bf9af8ff92995af0101b7136e5a6f908aaa59e8
RoyLiuShinetech <liurj@shinetechchina.com>
2013-09-05 18:29:24 +0800

    development too 2

--- RebasingDemo/ConsoleDemo/Program.cs
+++ RebasingDemo/ConsoleDemo/Program.cs
@@ -14,6 +14,7 @@ namespace ConsoleDemo
             Console.WriteLine("I am in development branch.");
             Console.WriteLine("Now I am in dev1 branch");
             Console.WriteLine("Now I am in development branch too");
+            Console.WriteLine("Now I am in development branch too 2");
         }
     }
 }
